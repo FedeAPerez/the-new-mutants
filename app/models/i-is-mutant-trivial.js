6c0845187a0a17ecbb8c616ea1c9b5bf43db107a
fedea <fedeaperez@hotmail.com.ar>
2019-02-17 21:16:15 -0300

    a espaniol y opts

--- app/models/i-is-mutant-trivial.js
+++ app/models/i-is-mutant-trivial.js
@@ -1,63 +1,59 @@
-// Pésima implementación, al recorrer otras direcciones fuera de la horizontal requerimos O(n*n) únicmanete para transformar la matrix
-const contains = (str, chars) => str.indexOf(chars) != -1;
+const contieneA = (str, chars) => str.indexOf(chars) != -1;
+
+const words = [
+    'CCCC',
+    'GGGG',
+    'TTTT',
+    'AAAA'
+];
+
+const contarOcurrenciasEnCadena = str => {
+    let count = 0;
+    if(contieneA(str, words[0]))
+        count++;
+    if(contieneA(str, words[1]))
+        count++;
+    if(contieneA(str, words[2]))
+        count++;
+    if(contieneA(str, words[3]))
+        count++;
+    return count;
+};
+
 module.exports = {
-    isMutant : function (dnaStr) {
-        let words = [
-            'CCCC',
-            'GGGG',
-            'TTTT',
-            'AAAA'
-        ];
-        
-        let length = dnaStr.length;
-        let ammountFounded = 0;
+    isMutant : function (dnaStr) {    
+        let cantidadCadenas = dnaStr.length;
+        let secuenciasEncontradas = 0;
         // descarto las palabras sin evaluar si la longitud es menor a 4 suponiendo N*N
-        if(length < 4) {
+        if(cantidadCadenas < 4) {
             return;
         } else {
             var index = 0;
             // busqueda horizontal O(n*2)
-            for(index; index < length; index++) {
-                if(contains(dnaStr[index], words[0]))
-                    ammountFounded++;
-                if(contains(dnaStr[index], words[1]))
-                    ammountFounded++;
-                if(contains(dnaStr[index], words[2]))
-                    ammountFounded++;
-                if(contains(dnaStr[index], words[3]))
-                    ammountFounded++;
+            for(index; index < cantidadCadenas; index++) {
+                secuenciasEncontradas += contarOcurrenciasEnCadena(dnaStr[index]);
                 // opt
-                if(ammountFounded > 1) {
+                if(secuenciasEncontradas > 1) {
                     return true;
                 }
             }
 
             // búsqueda vertical
-            for(var i = 0; i < length; i++) {
-                let verticalWord = '';
-                for(var j = 0; j < length; j++) {
-                    verticalWord += dnaStr[j][i];
+            for(var i = 0; i < cantidadCadenas; i++) {
+                let cadenaVertical = '';
+                for(var j = 0; j < cantidadCadenas; j++) {
+                    cadenaVertical += dnaStr[j][i];
                 }
-                if(contains(verticalWord, words[0]))
-                    ammountFounded++;
-                if(contains(verticalWord, words[1]))
-                    ammountFounded++;
-                if(contains(verticalWord, words[2]))
-                    ammountFounded++;
-                if(contains(verticalWord, words[3]))
-                    ammountFounded++;
+                secuenciasEncontradas += contarOcurrenciasEnCadena(cadenaVertical);
                     // opt
-                if(ammountFounded > 1) {
+                if(secuenciasEncontradas > 1) {
                     return true;
                 }
             }
             
-            // si se iteró sobre todas las cadenas
-            if(ammountFounded > 1) {
-                return true;
-            } else {
-                return false;
-            }
+            // si se iteró sobre todas las cadenas debo evaluar
+            // al llegar a este lugar no hay más de una ocurrencia
+            return false;
         }
     }
 }
