aac023f192207d515f414d39499f00422ba51fe6
fedea <fedeaperez@hotmail.com.ar>
2019-02-18 11:50:08 -0300

    all tests pass

--- app/models/i-is-mutant-trivial.js
+++ app/models/i-is-mutant-trivial.js
@@ -1,3 +1,4 @@
+// O(N)
 const contieneA = (str, chars) => str.indexOf(chars) != -1;
 
 const words = [
@@ -7,6 +8,7 @@ const words = [
     'AAAA'
 ];
 
+// Operación de O(N)
 const contarOcurrenciasEnCadena = str => {
     let count = 0;
     if(contieneA(str, words[0]))
@@ -24,16 +26,16 @@ const tieneSecuenciasMutantes = numSec => numSec > 1;
 
 module.exports = {
     isMutant : function (dnaStr) {    
-        let cantidadCadenas = dnaStr.length;
-        let secuenciasEncontradas = 0;
+        var cantidadCadenas = dnaStr.length;
+        var secuenciasEncontradas = 0;
         // descarto las palabras sin evaluar si la longitud es menor a 4 suponiendo N*N
         if(cantidadCadenas < 4) {
             return;
         } else {
-            var index = 0;
-            // busqueda horizontal O(n*2)
-            for(index; index < cantidadCadenas; index++) {
-                secuenciasEncontradas += contarOcurrenciasEnCadena(dnaStr[index]);
+
+            // busqueda horizontal en cadenas completas
+            for(let i; i < cantidadCadenas; i++) {
+                secuenciasEncontradas += contarOcurrenciasEnCadena(dnaStr[i]);
                 // opt
                 if(tieneSecuenciasMutantes(secuenciasEncontradas)) {
                     return true;
@@ -47,13 +49,14 @@ module.exports = {
                     cadenaVertical += dnaStr[j][i];
                 }
                 secuenciasEncontradas += contarOcurrenciasEnCadena(cadenaVertical);
-                    // opt
+                // opt
                 if(tieneSecuenciasMutantes(secuenciasEncontradas)) {
                     return true;
                 }
             }
-            // me quedo con los de apartir de 4
-            for( var k = 3 ; k < (cantidadCadenas * 2 - 4); k++ ) {
+
+            // me quedo con los diagonales de apartir de 4 - diagonales inferior a superior
+            for(var k = 3 ; k < (cantidadCadenas * 2 - 4); k++ ) {
                 let cadenaDiagonal = ''
                 for( var j = 0 ; j <= cantidadCadenas ; j++ ) {
                     var index = k - j;
@@ -61,7 +64,6 @@ module.exports = {
                         cadenaDiagonal += dnaStr[index][j];
                     }   
                 }
-                console.log(cadenaDiagonal);
                 secuenciasEncontradas += contarOcurrenciasEnCadena(cadenaDiagonal);
 
                 if(tieneSecuenciasMutantes(secuenciasEncontradas)) {
@@ -69,8 +71,34 @@ module.exports = {
                 }
             }
             
+            // opt para contar cadenas de a partir de 4 caracteres
+            for (var i = cantidadCadenas - 4; i > 0; i--) {
+                let cadenaDiagonalSuperior1 = '';
+                for (var j = 0, x = i; x <= cantidadCadenas - 1; j++, x++) {
+                    cadenaDiagonalSuperior1 += dnaStr[x][j];
+                }
+                secuenciasEncontradas += contarOcurrenciasEnCadena(cadenaDiagonalSuperior1);
+
+                if(tieneSecuenciasMutantes(secuenciasEncontradas)) {
+                    return true;
+                }
+            }
+         
+         
+            for (var i = 0; i <= cantidadCadenas - 1; i++) {
+                let cadenaDiagonalSuperior2 = ''
+                 for (var j = 0, y = i; y <= cantidadCadenas - 1; j++, y++) {
+                    cadenaDiagonalSuperior2 += dnaStr[j][y];
+                 }
+                secuenciasEncontradas += contarOcurrenciasEnCadena(cadenaDiagonalSuperior2);
+
+                if(tieneSecuenciasMutantes(secuenciasEncontradas)) {
+                    return true;
+                }
+            }
+
             // si se iteró sobre todas las cadenas debo evaluar
-            // al llegar a este lugar no hay más de una ocurrencia
+            // al llegar a este lugar no hay más de una ocurrencia, debería salir previamente
             return false;
         }
     }
--- test/mutant-test.js
+++ test/mutant-test.js
@@ -6,84 +6,83 @@ const app       = require('../config/app');
 describe('Mutant', function() {
     describe('POST', function() {
         
-        // it('200 - ejemplo meli', function(done) {
-        //     request(app)
-        //     .post('/mutant')
-        //     .set('Accept', 'application/json')
-        //     .send({
-        //         dna: [
-        //             "ATGCGA",
-        //             "CAGTGC",
-        //             "TTATGT",
-        //             "AGAAGG",
-        //             "CCCCTA",
-        //             "TCACTG"
-        //         ]
-        //     })
-        //     .expect(200)
-        //     .end((err) => {
-        //             if (err) return done(err);
-        //             done();
-        //         });
-        // });
+        it('200 - ejemplo meli', function(done) {
+            request(app)
+            .post('/mutant')
+            .set('Accept', 'application/json')
+            .send({
+                dna: [
+                    "ATGCGA",
+                    "CAGTGC",
+                    "TTATGT",
+                    "AGAAGG",
+                    "CCCCTA",
+                    "TCACTG"
+                ]
+            })
+            .expect(200)
+            .end((err) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
 
-        // it('200 - múltiples cadenas en diagonal principal y vertical final', function(done) {
-        //     request(app)
-        //     .post('/mutant')
-        //     .set('Accept', 'application/json')
-        //     .send({
-        //         dna: [
-        //             "ATCA",
-        //             "TACA",
-        //             "TCAA",
-        //             "TCGA"
-        //         ]
-        //     })
-        //     .expect(200)
-        //     .end((err) => {
-        //             if (err) return done(err);
-        //             done();
-        //         });
-        // });
+        it('200 - múltiples cadenas en diagonal principal y vertical final', function(done) {
+            request(app)
+            .post('/mutant')
+            .set('Accept', 'application/json')
+            .send({
+                dna: [
+                    "ATCA",
+                    "TACA",
+                    "TCAA",
+                    "TCGA"
+                ]
+            })
+            .expect(200)
+            .end((err) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
 
-        // it('403 - solamente una cadena horizontal', function(done) {
-        //     request(app)
-        //     .post('/mutant')
-        //     .set('Accept', 'application/json')
-        //     .send({
-        //         dna: [
-        //             "GGGG",
-        //             "ATCG",
-        //             "ATCG",
-        //             "CTGA"
-        //         ]
-        //     })
-        //     .expect(403)
-        //     .end((err) => {
-        //             if (err) return done(err);
-        //             done();
-        //         });
-        // });
+        it('403 - solamente una cadena horizontal', function(done) {
+            request(app)
+            .post('/mutant')
+            .set('Accept', 'application/json')
+            .send({
+                dna: [
+                    "GGGG",
+                    "ATCG",
+                    "ATCG",
+                    "CTGA"
+                ]
+            })
+            .expect(403)
+            .end((err) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
 
-        // it('200 - una cadena CCCC horizontal y una TTTT vertical', function(done) {
-        //     request(app)
-        //     .post('/mutant')
-        //     .set('Accept', 'application/json')
-        //     .send({
-        //         dna: [
-        //             "CCCCC",
-        //             "ATCGT",
-        //             "ATCGT",
-        //             "CTGAA",
-        //             "CTGAC"
-        //         ]
-        //     })
-        //     .expect(200)
-        //     .end((err) => {
-        //             if (err) return done(err);
-        //             done();
-        //         });
-        // });
+        it('200 - una cadena CCCC horizontal y una TTTT vertical', function(done) {
+            request(app)
+            .post('/mutant')
+            .set('Accept', 'application/json')
+            .send({
+                dna: [
+                    "CCCCC",
+                    "ATCGT",
+                    "ATCGT",
+                    "CTGAA",
+                    "CTGAC"
+                ]
+            })
+            .end((err) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
         
         it('200 - solamente una cadena diagonal y vertical', function(done) {
             request(app)
@@ -125,64 +124,85 @@ describe('Mutant', function() {
                 });
         });
 
-        // it('403 - 8*8 sin secuencia de caracteres repetidos', function(done) {
-        //     request(app)
-        //     .post('/mutant')
-        //     .set('Accept', 'application/json')
-        //     .send({
-        //         dna: [
-        //             "ATCGATCG",
-        //             "TACGTACG",
-        //             "TCAGTCAG",
-        //             "TGACTGAC", 
-        //             "ATCGATCG",
-        //             "TACGTACG",
-        //             "TCAGTCAG",
-        //             "TGACTGAC"]
-        //     })
-        //     .expect(403)
-        //     .end((err) => {
-        //             if (err) return done(err);
-        //             done();
-        //         });
-        // });
+        it('200 - cadenas diagonales izquierda superior hacia derecha inferior', function(done) {
+            request(app)
+            .post('/mutant')
+            .set('Content-Type', 'application/json')
+            .send({
+                dna: [
+                    'GTCGAT',
+                    'TGCGAC',
+                    'TCATCG',
+                    'TGCACG',
+                    'AAACAT',
+                    'TGACCA'
+                ]
+            })
+            .expect(200)
+            .end(err => {
+                if(err) return done(err);
+                done();
+            });
+        });
 
-        // it('403 - no llega a la cantidad de N', function(done) {
-        //     request(app)
-        //     .post('/mutant')
-        //     .set('Accept', 'application/json')
-        //     .send({
-        //         dna: [
-        //             "ATC",
-        //             "TAC",
-        //             "TCA"
-        //         ]
-        //     })
-        //     .expect(403)
-        //     .end((err) => {
-        //         if (err) return done(err);
-        //         done();
-        //     });
-        // });
+        it('403 - 8*8 sin secuencia de caracteres repetidos', function(done) {
+            request(app)
+            .post('/mutant')
+            .set('Accept', 'application/json')
+            .send({
+                dna: [
+                    "ATCGATCG",
+                    "TACGTACG",
+                    "TCAGTCAG",
+                    "TGACTGAC", 
+                    "ATCGATCG",
+                    "TACGTACG",
+                    "TCAGTCAG",
+                    "TGACTGAC"]
+            })
+            .expect(403)
+            .end((err) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
+
+        it('403 - no llega a la cantidad de N', function(done) {
+            request(app)
+            .post('/mutant')
+            .set('Accept', 'application/json')
+            .send({
+                dna: [
+                    "ATC",
+                    "TAC",
+                    "TCA"
+                ]
+            })
+            .expect(403)
+            .end((err) => {
+                if (err) return done(err);
+                done();
+            });
+        });
         
-        // it('200 - dos cadenas repetidas en interior', function(done) {
-        //     request(app)
-        //     .post('/mutant')
-        //     .set('Accept', 'application/json')
-        //     .send({
-        //         dna: [
-        //             "ATCGG",
-        //             "TACGC",
-        //             "TACAC",
-        //             "TAGCC", 
-        //             "GCCCC"
-        //         ]
-        //     })
-        //     .expect(200)
-        //     .end((err) => {
-        //         if (err) return done(err);
-        //         done();
-        //     });
-        // });
+        it('200 - dos cadenas repetidas en interior', function(done) {
+            request(app)
+            .post('/mutant')
+            .set('Accept', 'application/json')
+            .send({
+                dna: [
+                    "ATCGG",
+                    "TACGC",
+                    "TACAC",
+                    "TAGCC", 
+                    "GCCCC"
+                ]
+            })
+            .expect(200)
+            .end((err) => {
+                if (err) return done(err);
+                done();
+            });
+        });
     });
 });
